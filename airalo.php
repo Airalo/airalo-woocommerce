8829cd8d063f252aa3a49adfa31c16c8ba0876ad
andreasmaalouf <andreas.maalouf@airalo.com>
2024-04-24 12:41:53 +0200

    Feat: add main plugin file

---
+++ airalo.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Plugin Name: Airalo
+ * Plugin URI: https://airalo.com/
+ * Description: An ecommerce toolkit that helps you sell anything. Beautifully.
+ * Version: 1.0.0
+ * Author: Airalo
+ * Author URI: https://airalo.com
+ * Text Domain: test
+ * Domain Path: /i18n/languages/
+ * Requires at least: 6.3
+ * Requires PHP: 7.4
+ *
+ * @package WooCommerce
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'admin.php';
+
