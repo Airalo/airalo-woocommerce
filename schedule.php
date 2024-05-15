cdac855bdc74c7e0a0a8d7b7e4bc4ecb5865eaa1
andreasmaalouf <andreas.maalouf@airalo.com>
2024-05-15 11:19:51 +0200

    Feat: add scheduled event to call the syncer

---
+++ schedule.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+function schedule_hourly_event() {
+    if ( !wp_next_scheduled( 'hourly_cron_hook') ) {
+        wp_schedule_event(time(), 'hourly', 'hourly_cron_hook');
+    }
+}
+
+add_action( 'wp', 'schedule_hourly_event' );
+
+function sync_airalo_products_hourly() {
+
+    (new \Airalo\Admin\Syncers\ProductSyncer())->handle();
+
+}
+
+add_action( 'hourly_cron_hook', 'sync_airalo_products_hourly' );
