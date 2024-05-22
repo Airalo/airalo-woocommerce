dee9b8d8bace4f961f1a487c9d9a642f50d3b028
andreasmaalouf <andreas.maalouf@airalo.com>
2024-05-22 17:35:38 +0200

    Feat: delete all posts, their meta, and image when env is switched

--- src/Admin/Syncers/ProductSyncer.php
+++ src/Admin/Syncers/ProductSyncer.php
@@ -23,6 +23,11 @@ class ProductSyncer {
         ini_set('max_execution_time', self::AIRALO_MAX_EXECUTION);
 
         $options = new Option();
+
+        if ( $options->fetch_option( Option::ENVIRONMENT_SWITCHED ) == 'true' ) {
+            $this->removeAllAiraloProducts( $options );
+        }
+
         $environment = $options->fetch_option( Option::USE_SANDBOX ) == Option::ENABLED ? 'sandbox' : 'production';
 
         $credential = new Credential();
@@ -90,5 +95,60 @@ class ProductSyncer {
         }
 
         $options->insert_option( Option::SYNC_ERROR, $error );
+    }
+
+    private function removeAllAiraloProducts(Option $option): void  {
+        $args = [
+            'post_type' => 'product',
+            'posts_per_page' => -1,
+            'meta_query' => [
+                [
+                    'key' => '_sku',
+                    'value' => Product::SKU_PREFIX,
+                    'compare' => 'LIKE',
+                ],
+            ],
+        ];
+
+        $query = new \WP_Query($args);
+
+        $count = 0;
+        if ( $query->have_posts() ) {
+            while ( $query->have_posts() ) {
+                $query->the_post();
+                $product_id = get_the_ID();
+                $product = wc_get_product( $product_id );
+
+                $taxonomy_name = Term::IMAGE_NAME_PREFIX . $product->get_attribute( 'operator_id' );
+
+                $labels = [
+                    'name' => _x( $taxonomy_name, 'taxonomy general name', 'textdomain' ),
+                    'singular_name' => _x( $taxonomy_name.'_singular', 'taxonomy singular name', 'textdomain' ),
+                ];
+
+                $args = [
+                    'labels' => $labels,
+                    'show_ui' => true,
+                    'show_admin_column' => true,
+                    'query_var' => true,
+                    'rewrite' => ['slug' => $taxonomy_name],
+                ];
+
+                register_taxonomy( $taxonomy_name, ['post'], $args );
+
+                $term_name = $taxonomy_name .'_id';
+                $term = get_term_by( 'slug', $term_name,  $taxonomy_name );
+
+                $image_id = get_term_meta( $term->term_id, Term::IMAGE_METADATA_KEY, true );
+
+                wp_delete_term( $term->term_id, $taxonomy_name );
+
+                wp_delete_post( $product_id, true );
+                wp_delete_post( $image_id, true );
+            }
+            wp_reset_postdata();
+        }
+
+        $option->insert_option( Option::ENVIRONMENT_SWITCHED, 'false' );
     }
 }
