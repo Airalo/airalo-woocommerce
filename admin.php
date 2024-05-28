30e7ebb5c810ce6d6ffcf92c94ad55d46e937aa5
MiroIsikiyski <miroslav.isikiyski@airalo.com>
2024-05-28 12:31:00 +0300

    snake case

--- admin.php
+++ admin.php
@@ -308,13 +308,13 @@ function sync_products_function() {
     $product_syncer->handle();
 }
 
-add_filter('woocommerce_add_to_cart_validation', 'validate_cart_item_quantity', 10, 3);
+add_filter( 'woocommerce_add_to_cart_validation', 'validate_cart_item_quantity', 10, 3 );
 
 function validate_cart_item_quantity( $passed, $ignore_param, $quantity ) {
     return (new AiraloOrder)->handle_validation( $passed, $quantity );
 }
 
-add_action( 'woocommerce_thankyou', 'airalo_submit_order', 10, 1);
+add_action( 'woocommerce_thankyou', 'airalo_submit_order', 10, 1 );
 
 function airalo_submit_order( $order ) {
     (new AiraloOrder)->handle_order( $order );
