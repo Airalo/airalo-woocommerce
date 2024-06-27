cb927b9210ca1982075ccf8d5f4e528876792ec5
andreasmaalouf <andreas.maalouf@airalo.com>
2024-06-27 10:53:02 +0200

    Chore: use wp_enqueue_style for stylesheet

--- includes/airalo_admin.php
+++ includes/airalo_admin.php
@@ -52,6 +52,8 @@ function airalo_settings_page () {
 
     $language = $options->fetch_option( \Airalo\Admin\Settings\Option::LANGUAGE );
 
+    wp_enqueue_style('airalo-admin-style', 'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+
     ?>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
