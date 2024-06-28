eebd538f07c97b7b2b5aafbfa40679dc8c2895bf
andreasmaalouf <andreas.maalouf@airalo.com>
2024-06-28 09:15:47 +0200

    Fix: remove style sheet from html

--- includes/airalo_admin.php
+++ includes/airalo_admin.php
@@ -60,7 +60,6 @@ function airalo_settings_page () {
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
     <style><?php require plugin_dir_path( __FILE__ ) . '../assets/css/resetStyle.css'; ?></style>
     <style><?php require plugin_dir_path( __FILE__ ) . '../assets/css/pluginStyle.css'; ?></style>
