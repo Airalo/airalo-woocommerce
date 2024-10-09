da5af2585bc43f41eec078fbde7d6735cd8d0b9a
Alex Sh <oleksandr.shapoval@airalo.com>
2024-10-09 18:32:05 +0300

    fix my esim page

--- includes/my_esim.php
+++ includes/my_esim.php
@@ -7,9 +7,72 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @return string
  */
 function airalo_main() {
+    $allowed_tags = [
+        'div' => [
+            'class' => [],
+            'id' => [],
+        ],
+        'p' => [
+            'class' => [],
+            'id' => [],
+        ],
+        'ul' => [
+            'class' => [],
+            'id' => [],
+        ],
+        'li' => [
+            'class' => [],
+            'id' => [],
+        ],
+        'a' => [
+            'href' => [],
+            'class' => [],
+            'id' => [],
+        ],
+        'input' => [
+            'type' => [],
+            'name' => [],
+            'id' => [],
+            'class' => [],
+            'checked' => [],
+            'value' => [],
+        ],
+        'label' => [
+            'for' => [],
+            'class' => [],
+            'id' => [],
+        ],
+        'select' => [
+            'name' => [],
+            'id' => [],
+            'class' => [],
+            'onchange' => []
+        ],
+        'option' => [
+            'value' => [],
+        ],
+        'img' => [
+            'src' => [],
+            'width' => [],
+            'height' => [],
+            'alt' => [],
+            'id' => [],
+            'decoding' => [],
+        ],
+        'span' => [
+            'class' => [],
+            'style' => [],
+            'id' => [],
+        ],
+        'strong' => [],
+        'em' => [],
+    ];
+    $allowed_protocols = wp_allowed_protocols();
+    $allowed_protocols[] = 'data';
+
     $html_content = ( new \Airalo\User\MySimsPageBuilder() )->build_html();
 
-    return wp_kses_post( $html_content );
+    return wp_kses( $html_content, $allowed_tags, $allowed_protocols );
 }
 
 add_shortcode( 'airalo_woocommerce_my_esim', 'airalo_main' );
